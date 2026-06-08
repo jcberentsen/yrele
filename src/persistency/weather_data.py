1841f0c78cd808a3e337dfb63f81213d653c19b6
Christian Berentsen <jc.berentsen@gmail.com>
2026-06-08 18:31:41 +0200

    feat: Sketch tables

--- src/persistency/weather_data.py
+++ src/persistency/weather_data.py
@@ -4,6 +4,16 @@ import sqlite3
 
 def main():
     con = sqlite3.connect("localweather.db")
+    with con:
+        cur = con.cursor()
+        cur.execute("CREATE TABLE IF NOT EXISTS user (id, name)")
+        cur.execute("CREATE TABLE IF NOT EXISTS userlocations (userid, locationid)")
+        cur.execute("CREATE TABLE IF NOT EXISTS locations (locationid, lat, lon)")
+        cur.execute("CREATE TABLE IF NOT EXISTS weather (locationid, timestamp, report)")
+
+        # check tables
+        res = cur.execute("SELECT name FROM sqlite_master")
+        print(res.fetchall())
 
 if __name__ == "__main__":
     main()
