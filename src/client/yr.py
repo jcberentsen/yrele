59d34f6af40098ad1f111b816160dd4447424051
Christian Berentsen <jc.berentsen@gmail.com>
2026-06-08 17:38:43 +0200

    feat: Use the real met api
    yr.py will request the forecast for a fixed location (Oslo)
    Currently it will only print the response status code (200 ok)

--- src/client/yr.py
+++ src/client/yr.py
@@ -1,7 +1,22 @@
 #!/usr/bin/env python3
 
+import requests
+import urllib.parse
+
+def locationQuery(lat, lon):
+    data = {
+        "lat": lat,
+        "lon": lon
+    }
+    return urllib.parse.urlencode(data)
+
 def main():
-    pass
+    query = locationQuery("59.9139", "10.7522")
+
+    yrl = 'https://api.met.no/weatherapi/locationforecast/2.0/compact?' + query
+    # yrl = "https://api.met.no/weatherapi/locationforecast/2.0/compact?lat={lat}&lon={lon}"
+    r = requests.get(yrl)
+    print(r)
 
 if __name__ == "__main__":
     main()
