2b28c958ed3dab34aec527e15cee9a17dc7ba0f6
Christian Berentsen <jc.berentsen@gmail.com>
2026-06-09 13:11:41 +0200

    feat: Integrate PersistencySqlite

--- src/common/persist_sqlite.py
+++ src/common/persist_sqlite.py
@@ -1,8 +1,18 @@
 #!/usr/bin/env python3
 
+import sqlite3
+
 class PersistencySqlite:
     def __init__(self):
-        pass
+        self.con = sqlite3.connect("localweather.db")
+        cur = self.con.cursor()
+        # TODO for migration cur.execute("CREATE TABLE IF NOT EXISTS schema_version (version)")
+        cur.execute("CREATE TABLE IF NOT EXISTS locations (locationid, lat, lon)")
+        cur.execute("CREATE TABLE IF NOT EXISTS weather (locationid, timestamp, report)")
+
+        # check tables
+        res = cur.execute("SELECT name FROM sqlite_master")
+        print(res.fetchall())
 
     def list_locations(self):
         # TODO
--- src/persistency/weather_data.py
+++ src/persistency/weather_data.py
@@ -1,18 +1,9 @@
 #!/usr/bin/env python3
 
-import sqlite3
+from src.common.persist_sqlite import PersistencySqlite
 
 def main():
-    con = sqlite3.connect("localweather.db")
-    with con:
-        cur = con.cursor()
-        # TODO for migration cur.execute("CREATE TABLE IF NOT EXISTS schema_version (version)")
-        cur.execute("CREATE TABLE IF NOT EXISTS locations (locationid, lat, lon)")
-        cur.execute("CREATE TABLE IF NOT EXISTS weather (locationid, timestamp, report)")
-
-        # check tables
-        res = cur.execute("SELECT name FROM sqlite_master")
-        print(res.fetchall())
+    persist = PersistencySqlite()
 
 if __name__ == "__main__":
     main()
