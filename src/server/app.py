d45a2e0a3230bcadb9c798e187eebc234c929769
Christian Berentsen <jc.berentsen@gmail.com>
2026-06-09 07:03:05 +0200

    feat: Add fastapi lifespan stub

--- src/server/app.py
+++ src/server/app.py
@@ -1,12 +1,22 @@
 #!/usr/bin/env python3
 
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastApi):
+    db = None # TODO
+    print("Yielding a db for use by handler")
+    yield {"db": db}
+
+    # TODO clean up db
+    print("Cleaning up db pool...")
+
+app = FastAPI(lifespan=lifespan)
 
 @app.get("/")
 async def read_root():
-    return {"status": "success", "message": "Welcome to a modern Python HTTP server"}
+    return {"status": "success", "message": "This is an api server for weather data (not a teapot)"}
 
 @app.get("/items/{item_id}")
 async def read_item(item_id: int, q: str = None):
