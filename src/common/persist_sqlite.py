fffb48645743fcfd3d7c6680c689f6a3d4c21d43
Christian Berentsen <jc.berentsen@gmail.com>
2026-06-10 09:35:41 +0200

    refactor: Tidy location storage
    We can use the implicit rowid, and embed the id in the Location itself
    for reference

--- src/common/persist_sqlite.py
+++ src/common/persist_sqlite.py
@@ -2,40 +2,48 @@
 
 import sqlite3
 
+from src.service.location import Location
+
+def toLocation(tupled):
+    return Location(id=tupled[0], name=tupled[1], lat= tupled[2], lon= tupled[3])
+
 class PersistencySqlite:
     def __init__(self):
         self.con = sqlite3.connect("localweather.db")
         cursor = self.con.cursor()
-        cursor.execute("CREATE TABLE IF NOT EXISTS locations (locationid INTEGER PRIMARY KEY, lat, lon)")
-        cursor.execute("CREATE TABLE IF NOT EXISTS weather (locationid, timestamp, report)")
+        cursor.execute("CREATE TABLE IF NOT EXISTS locations (name, lat, lon)")
+        cursor.execute("CREATE TABLE IF NOT EXISTS weather (locationid, timestamp TEXT DEFAULT CURRENT_TIMESTAMP, report)")
 
         # check tables
         cursor.execute("SELECT name FROM sqlite_master")
         print(cursor.fetchall())
 
+    def add_location(self, location):
+        cursor = self.con.cursor()
+        cursor.execute("INSERT INTO locations (name, lat,lon) VALUES (?,?,?)", (location.name, location.lat, location.lon,))
+        location_id = cursor.lastrowid
+        self.commit()
+        return {"created": location_id, "locations": self.list_locations()}
+
     def list_locations(self):
         cursor = self.con.cursor()
-        cursor.execute("SELECT * FROM locations")
+        cursor.execute("SELECT rowid, * FROM locations")
         items = cursor.fetchall()
-        print(items)
-        return list(items) # TODO transform to Location values
+        locations = list(map(toLocation, items))
+        print(locations)
+        return locations
 
     def commit(self):
         self.con.commit()
 
     def delete_location(self, location_id):
         cursor = self.con.cursor()
-        cursor.execute("DELETE FROM locations WHERE locationid=?", (location_id,))
+        cursor.execute("DELETE FROM locations WHERE rowid=?", (location_id,))
         self.commit()
         return self.list_locations()
 
-    def add_location(self, location):
-        cursor = self.con.cursor()
-        cursor.execute("INSERT INTO locations (lat,lon) VALUES (?,?)", (location.lat, location.lon,))
-        location_id = cursor.lastrowid
-        self.commit()
-        return {"created": location_id, "locations": self.list_locations()}
 
-    def store_observation(location, weather_data):
-        # TODO
-        pass
+    def store_observation(self, location, weather_data):
+        cursor = self.con.cursor()
+        cursor.execute("INSERT INTO weather (locationid, report) VALUES (?,?)", (location.id, weather_data,))
+        self.commit()
--- src/service/location.py
+++ src/service/location.py
@@ -3,17 +3,20 @@
 from pydantic import BaseModel, Field
 
 class Location(BaseModel):
+    id: int
     name: str = "oslo"
     lat: str = "59.913"
     lon: str = "10.7522"
 
 oslo: Location = Location(
+    id = 0,
     name="Oslo",
     lat="59.9139",
     lon="10.7522",
 )
 
 kongsvinger: Location = Location(
+    id = 1,
     name="Kongsvinger",
     lat="60.1898",
     lon="11.9901",
